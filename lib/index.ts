9bd2ccc09ad5260f37ffc3c5e77b6e0a6e5d576b
Thomas Meadows <thomas@itpro.tv>
2017-05-20 08:43:38 -0400

    updated server to not need to declare policies

--- lib/index.ts
+++ lib/index.ts
@@ -1,63 +1,52 @@
-const bodyParser = require('body-parser');
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
 
 export function DExpress(): any {
-  return function(OriginalClassConstructor: any) {
+  return function(originalClassConstructor: new () => {}) {
+    return class extends originalClassConstructor {
+      public __seatbelt__: string;
+      public __seatbelt_strap__: Function;
+      constructor() {
+        super();
+        this.__seatbelt__ = 'server';
+        this.__seatbelt_strap__ = function(routes: any[]) {
+          this.server = express();
+          this.port = process.env.port || 3000;
+          this.server.use(bodyParser.json());
+          this.__controller_wrapper__ = function (route: any, req: any, res: any, next: Function) {
+            route.controller({
+              next,
+              send: (...params: any[]) => res.send(...params),
+              params: Object.assign(
+                {},
+                typeof req.params === 'object' ? req.params : {},
+                typeof req.body === 'object' ? req.body : {}
+                ,
+                typeof req.query === 'object' ? req.query : {}
+              )
+            }, {
+              req,
+              res,
+              next
+            });
+          };
 
-    return function () {
-      const origin = new OriginalClassConstructor();
-      origin.__seatbelt__ = 'server';
-      origin.__seatbelt_strap__ = function(classesByType: any) {
-        origin.express = require('express');
-        origin.app = origin.express();
-        origin.port = process.env.port || 3000;
-        origin.app.use(bodyParser.json());
-        origin.__controller_wrapper__ = function (controllerFunction: Function, req: any, res: any, next: Function) {
-          controllerFunction({
-            next,
-            send: (...params: any[]) => res.send(...params),
-            params: Object.assign(
-              {},
-              typeof req.params === 'object' ? req.params : {},
-              typeof req.body === 'object' ? req.body : {}
-              ,
-              typeof req.query === 'object' ? req.query : {}
-            )
-          }, {
-            req,
-            res,
-            next
+          if (routes && Array.isArray(routes)) {
+            routes.forEach((route: any) => {
+              route['__seatbelt_config__'].type.forEach((eachType: string) => {
+                route['__seatbelt_config__'].path.forEach((eachPath: string) => {
+                  this.server[eachType.toLowerCase()](eachPath, (req: any, res: any, next: Function) => this.__controller_wrapper__(route, req, res, next));
+                });
+              });
+            });
+          }
+
+          this.server.listen(this.port, () => {
+            console.log(`Example app listening on port ${this.port}!`);
           });
+
         };
-
-        if (classesByType['route']) {
-          classesByType['route'].forEach((route: any) => {
-            const policies: any[] = [];
-            route.__seatbelt_config__.policies.forEach((routePolicyName: string) => {
-              classesByType['policy'].forEach((policy: any) => {
-                if (routePolicyName === policy.__name__) {
-                  policies.push((req: any, res: any, next: Function) => origin.__controller_wrapper__(policy.controller, req, res, next));
-                }
-              });
-            });
-
-            const policiesPlusController = [
-              ...policies,
-              (req: any, res: any, next: Function) => origin.__controller_wrapper__(route.controller, req, res, next)
-            ];
-            route['__seatbelt_config__'].type.forEach((eachType: string) => {
-              route['__seatbelt_config__'].path.forEach((eachPath: string) => {
-                origin.app[eachType.toLowerCase()](eachPath, ...policiesPlusController);
-              });
-            });
-          });
-        }
-
-        origin.app.listen(origin.port, () => {
-          console.log(`Example app listening on port ${origin.port}!`);
-        });
-
       };
-      return origin;
     };
   };
 }
