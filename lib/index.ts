2312f19ca63c7aa1c3234af059e6bb8cef0519c4
Thomas Meadows <thomasmeadows@gmail.com>
2017-06-28 10:02:55 -0400

    added more responses

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import { ServerPlugin } from '@seatbelt/core/plugins';
+import { Request, Response } from '@seatbelt/core';
 import { Log } from '@seatbelt/core';
 
 export interface IServerConfig {
@@ -25,14 +26,20 @@ export class ExpressServer implements ServerPlugin.BaseServer {
 
   public conformServerControllerToSeatbeltController: Function = function (route: ServerPlugin.Route, req: express.Request, res: express.Response) {
 
-    const seatbeltResponse: ServerPlugin.Response = {
-      send: (status: number, body: Object) => {
-        res.status(status);
-        return res.send(body);
-      }
+    const send = (status: number, body: Object) => res.status(status).send(body);
+
+    const seatbeltResponse: Response.Base = {
+      send,
+      ok: (body: Object) => send(200, body),
+      created: (body: Object) => send(201, body),
+      badRequest: (body: Object) => send(400, body),
+      unauthorized: (body: Object) => send(401, body),
+      forbidden: (body: Object) => send(403, body),
+      notFound: (body: Object) => send(404, body),
+      serverError: (body: Object) => send(500, body)
     };
 
-    const seatbeltRequest: ServerPlugin.Request = {
+    const seatbeltRequest: Request.Base = {
       allParams: Object.assign(
         {},
         typeof req.query === 'object' ? req.query : {},
@@ -59,7 +66,7 @@ export class ExpressServer implements ServerPlugin.BaseServer {
       routes.forEach((route: ServerPlugin.Route) => {
         route['__routeConfig'].type.forEach((eachType: string) => {
           route['__routeConfig'].path.forEach((eachPath: string) => {
-            this.server[eachType.toLowerCase()](eachPath, (req: express.Request, res: express.Response) => this.conformExpressControllerToSeatbeltController(route, req, res));
+            this.server[eachType.toLowerCase()](eachPath, (req: express.Request, res: express.Response) => this.conformServerControllerToSeatbeltController(route, req, res));
           });
         });
       });
